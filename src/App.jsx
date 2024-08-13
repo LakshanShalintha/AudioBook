23819feac4420d3d38079630758eb24e286ca375
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-08-14 00:04:05 +0530

    Add onboarding page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Onboard from './Pages/Common/Onboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Onboard />
+    </div>
+  );
+}
+
+export default App;
